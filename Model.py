e1e5e40628837993b9d77f0c4a32a6639e45ac08
harshsonar1998 <33555889+harshsonar1998@users.noreply.github.com>
2025-08-07 09:52:50 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,20 +1,18 @@
 import arcpy
 
-
-Roads_ND_nd = "Your Road Network path"
+"your RoadNetwork Name" = "Your Road Network path"
 Closest_Facility = "Closest Facility"
 Closest_Facility__5_ = Closest_Facility
-Merged_shp = "Your Facilities Shape FilePath"
+"Your Exported Facilities ShapeFile Name" = "Your Facilities ShapeFile Path"
 Closest_Facility__2_ = Closest_Facility__5_
-Export_Output_shp = "Your Incident ShapeFile Path"
+"Your Exported Incidents ShapeFile Name" = "Your Incident ShapeFile Path"
 Closest_Facility__3_ = Closest_Facility__2_
 Solve_Succeeded = "true"
 
 arcpy.MakeClosestFacilityLayer_na(Roads_ND_nd, "Closest Facility", "Minutes", "TRAVEL_TO", "", "1", "Length_mi;Minutes", "ALLOW_UTURNS", "Oneway", "NO_HIERARCHY", "", "TRUE_LINES_WITH_MEASURES", "", "NOT_USED")
 
-arcpy.AddLocations_na(Closest_Facility, "Facilities", Merged_shp, "", "5000 Meters", "", "Roads SHAPE;Roads_ND_Junctions NONE", "MATCH_TO_CLOSEST", "APPEND", "NO_SNAP", "5 Meters", "INCLUDE", "Roads #;Roads_ND_Junctions #")
+arcpy.AddLocations_na(Closest_Facility, "Facilities", "Your Exported Facilities ShapeFile name" , "", "5000 Meters", "", "Roads SHAPE;Roads_ND_Junctions NONE", "MATCH_TO_CLOSEST", "APPEND", "NO_SNAP", "5 Meters", "INCLUDE", "Roads #;Roads_ND_Junctions #")
 
-arcpy.AddLocations_na(Closest_Facility__5_, "Incidents", Export_Output_shp, "Name Name #", "5000 Meters", "", "Roads SHAPE;Roads_ND_Junctions NONE", "MATCH_TO_CLOSEST", "APPEND", "NO_SNAP", "5 Meters", "INCLUDE", "Roads #;Roads_ND_Junctions #")
+arcpy.AddLocations_na(Closest_Facility__5_, "Incidents", "Your Exported Incidents ShapeFile name" , "Name Name #", "5000 Meters", "", "Roads SHAPE;Roads_ND_Junctions NONE", "MATCH_TO_CLOSEST", "APPEND", "NO_SNAP", "5 Meters", "INCLUDE", "Roads #;Roads_ND_Junctions #")
 
 arcpy.Solve_na(Closest_Facility__2_, "SKIP", "TERMINATE", "", "")
-
